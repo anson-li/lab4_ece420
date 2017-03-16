dd22617040bb6736d592d86b9e3dc4e8d13be701
anson-li <ansonli.94@gmail.com>
2017-03-16 14:36:40 -0600

    update main to be usable

--- main.c
+++ main.c
@@ -1,11 +1,5 @@
 /*
-    Test the result stored in "data_output" against a serial implementation.
-
-    -----
-    Compiling:
-    Include "Lab4_IO.c" to compile. Set the macro "LAB4_EXTEND" defined in the "Lab4_IO.c" file to include the extended functions
-    $ gcc serialtester.c Lab4_IO.c -o serialtester -lm 
-
+    Tests process in parallel.
     -----
     Return values:
     0      result is correct
@@ -41,27 +35,11 @@ int main (int argc, char* argv[]){
     double error;
     FILE *fp;
 
-    // Load the data and simple verification
-    if ((fp = fopen("data_output", "r")) == NULL ){
-      printf("Error loading the data_output.\n");
-        return 253;
-    }
-    fscanf(fp, "%d\n%lf\n", &collected_nodecount, &error);
-    if (get_node_stat(&nodecount, &num_in_links, &num_out_links)) return 254;
-    if (nodecount != collected_nodecount){
-        printf("Problem size does not match!\n");
-        free(num_in_links); free(num_out_links);
-        return 2;
-    }
-    collected_r = malloc(collected_nodecount * sizeof(double));
-    for ( i = 0; i < collected_nodecount; ++i)
-        fscanf(fp, "%lf\n", &collected_r[i]);
-    fclose(fp);
-
     // Adjust the threshold according to the problem size
     cst_addapted_threshold = THRESHOLD;
     
     // Calculate the result
+    if (get_node_stat(&nodecount, &num_in_links, &num_out_links)) return 254;
     if (node_init(&nodehead, num_in_links, num_out_links, 0, nodecount)) return 254;
     
     r = malloc(nodecount * sizeof(double));
@@ -70,7 +48,7 @@ int main (int argc, char* argv[]){
         r[i] = 1.0 / nodecount;
     damp_const = (1.0 - DAMPING_FACTOR) / nodecount;
     // CORE CALCULATION
-    do{
+    do {
         ++iterationcount;
         vec_cp(r, r_pre, nodecount);
         for ( i = 0; i < nodecount; ++i){
@@ -80,22 +58,13 @@ int main (int argc, char* argv[]){
             r[i] *= DAMPING_FACTOR;
             r[i] += damp_const;
         }
-    }while(rel_error(r, r_pre, nodecount) >= EPSILON);
+    } while(rel_error(r, r_pre, nodecount) >= EPSILON);
     //printf("Program converges at %d th iteration.\n", iterationcount);
+
+    Lab4_saveoutput(r, nodecount, 0);
 
     // post processing
     node_destroy(nodehead, nodecount);
     free(num_in_links); free(num_out_links);
-    
-    // Compare the result 
-    error = rel_error(collected_r, r, nodecount);
-    printf ("The relative error against the reference result is %e .\n", error);
-    free(r); free(r_pre); free(collected_r);
-    if ( error < cst_addapted_threshold){
-        printf("Congratulations! Your result is correct!\n");
-        return 0;
-    }else{
-        printf("Sorry. Your result is wrong.\n");
-        return 1;
-    }
+
 }
